cb4d9d41a17f02af85551b9b8654d9ed061be0b6
bidr <badr.jebbari3615@gmail.com>
2023-12-01 20:29:31 +0100

    first part

---
+++ src/day1/part2/Main.java
@@ -0,0 +1,36 @@
+package day1.part2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+    static FileInputStream in;
+    public static void main(String[] args) throws IOException {
+        in = new FileInputStream("C:\\Users\\bidr\\Desktop\\AdventOfCode2023\\src\\day1\\part1\\input.txt");
+        System.out.println(solve());
+    }
+
+    public static int solve() throws IOException {
+        int res = 0;
+        int firstDigit = -1, lastDigit = -1;
+
+        int c;
+        while ((c = in.read()) != -1) {
+
+
+
+            if (Character.isDigit(c)) {
+                if (firstDigit == -1)
+                    firstDigit = c - '0';
+                lastDigit = c - '0';
+            }
+
+            if (c == '\n') {
+                res += firstDigit * 10 + lastDigit;
+                firstDigit = -1;
+            }
+
+        }
+        return res;
+    }
+}
